f68302dd0ba15e37e06aabfc6db7f6ccd711e9a3
Lei <107736675+xiaoaleiBLUE@users.noreply.github.com>
2023-08-08 16:33:43 +0800

    Update 11.use_const.cpp

--- C++进阶语法--OOP,STL/11.use_const.cpp
+++ C++进阶语法--OOP,STL/11.use_const.cpp
@@ -1,8 +1,22 @@
+/*
+            在类中使用 const
+常函数
+    函数名称后加const
+    函数体内不可以修改类成员属性
+
+常对象
+    声明对象时前面加const
+    不可以修改常对象的成员属性
+    仅能调用常函数
+
+*/
+
 #include<iostream>
 #include<string>
 #include<vector>
 
 using namespace std;
+
 
 class Account
 {
@@ -13,14 +27,15 @@ private:
 public:
     string name {"account"};
 
-    // 设置新名称, 修改名字
-    void set_new_name(string new_name) const {
+    // 设置修改名字函数, const 修饰, 就不能修改
+    void set_new_name(string new_name) const 
+    {
 
-        // name = new_name;
     }
 
-    // 获取名字 --> get_name() 变成常函数
-    string get_name() const {
+    // 获取名字函数
+    string get_name() const 
+    {
 
         return name;
     }
@@ -50,24 +65,35 @@ Account::~Account()
 
 
 
+// 主函数
 int main()
 {
-    // 常函数
-    // Account alice_account {"Alice", 1000};
+    Account alice_account {"Alice", 1000};
 
-    // alice_account.set_new_name ("Alice2");             // 由于是 const, 名字不会被修改
+    alice_account.set_new_name ("Alice2");                // 由于是 const, 名字不会被修改
 
-    // cout << alice_account.get_name() << endl;          // Alice
+    cout << alice_account.get_name() << endl;             // Alice, 输出仍是 Alice
 
+    // string name 是 public 属性, 可以进行直接属性修改
+    Account bob_account {"Bob", 2000.0};
+    bob_account.name = "Bob2";                            // 修改名字
+    cout << bob_account.get_name() << endl;               // Bob2
 
-    // 常对象 --> 只能访问常函数
+    // 常对象, 代表变量不可发生改动
     const Account bob_account {"Bob", 2000.0};
-    // bob_account.name = "Bob2";                         // 修改名字
-    // cout << bob_account.get_name() << endl;            // Bob2
 
-    cout << bob_account.get_name() << endl;
-    
+    cout << bob_account.get_name() << endl;               // 必须修改 string get_name() 为 string get_name() const
+
 
     return 0;
 
 }
+
+
+
+
+
+
+
+
+
