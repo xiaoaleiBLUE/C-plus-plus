8176e0c6b7f4d45a45f0b17ee60428b7fc3cf302
Lei <107736675+xiaoaleiBLUE@users.noreply.github.com>
2023-08-08 16:59:36 +0800

    Update 12.static_use.cpp

--- C++进阶语法--OOP,STL/12.static_use.cpp
+++ C++进阶语法--OOP,STL/12.static_use.cpp
@@ -1,3 +1,25 @@
+/*
+            在类中使用 static
+静态成员变量
+    所有对象共有同一份数据(数据共享)
+    在类中声明, 类外初始化
+
+静态成员函数
+    所有对象共享同一函数
+    只能访问静态成员变量
+
+class Account
+{
+private:
+    static int num_accounts;                  // 静态成员变量
+
+public:
+    static int get_num_accounts();            // 静态成员函数
+
+};
+
+*/
+
 #include<iostream>
 #include<string>
 #include<vector>
@@ -5,74 +27,22 @@
 using namespace std;
 
 
-// class Account
-// {
-// private:
-//     string name {"account"};
-//     double balance {0.0};
-
-// public:
-//     static int num_accounts;                        // 静态成员变量, 类外初始化
-//     Account(string name, double balance);
-//     ~Account();
-
-// };
-
-
-// // static 成员变量类外初始化
-// int Account :: num_accounts {0};
-
-// Account :: Account(string name, double balance)
-//     : name {name}, balance {balance}
-//     {
-//         num_accounts++;
-//     }
-
-
-// Account :: ~Account()
-// {
-//     num_accounts--;
-// }
-
-
-// // 一般不这样进行访问
-// int main()
-// {
-//     cout << Account :: num_accounts << endl;
-
-//     // 创建一个对象
-//     Account alice_account {"Alice_account", 2000.0};
-
-//     // 访问 static 成员变量
-//     cout << alice_account.num_accounts << endl;
-
-//     // 访问 static 成员变量
-//     cout << Account :: num_accounts << endl;
-
-
-
-
-//     return 0;
-// }
-
-
-// 这样访问静态成员变量
-
+// 
 class Account
 {
 private:
     string name {"account"};
     double balance {0.0};
-    static int num_accounts;                         // 静态成员变量, 类外初始化
+    static int num_accounts;                         // 静态成员变量, 需要: 类外初始化
 
 public:
-    static int get_num_accounts();                   // 一般是创建一个方法去访问静态成员变量
+    static int get_num_accounts();                   // 一般是创建一个方法去访问静态成员变量, 静态成员函数
     Account(string name, double balance);
     ~Account();
 };
 
 
-// 静态成员变量类外初始化
+// 静态成员变量, 类外初始化
 int Account :: num_accounts {0};
 
 // 定义方法访问 private 中的静态成员变量
@@ -100,19 +70,30 @@ Account::~Account()
 
 int main()
 {
-    cout << Account :: get_num_accounts() << endl;
+    cout << Account :: get_num_accounts() << endl;                    // 0
 
     // 定义对象
     Account alice_account {"alice_account", 1000.0};
 
-    cout << Account :: get_num_accounts() << endl;
+    cout << Account :: get_num_accounts() << endl;                    // 1
 
     // 定义对象
     Account bob_account {"bob_account", 500.0};
     
-    cout << Account :: get_num_accounts() << endl;
+    cout << Account :: get_num_accounts() << endl;                    // 2
 
 
     return 0; 
 
 }
+
+
+
+
+
+
+
+
+
+
+
